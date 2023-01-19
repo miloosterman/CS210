ba43b2f95366083d7ec2c0481d32f35845b70315
miloosterman <miloosterman@protonmail.com>
2023-01-19 11:33:54 -0700

    Built main and created battleship board array

---
+++ Battleship.java
@@ -0,0 +1,13 @@
+
+public class Battleship{
+    public static void main(String[] args){
+        String[][] gameBoard = {
+            {"A0", "A1", "A2", "A3", "A4"},
+            {"B0", "B1", "B2", "B3", "B4"},
+            {"C0", "C1", "C2", "C3", "C4"},
+            {"D0", "D1", "D2", "D3", "D4"},
+            {"E0", "E1", "E2", "E3", "E4"},
+        };
+        System.out.print(gameBoard[4][4]);
+    }
+}
