5e8bb7539454fa3e159116fea39ad4a0821feffd
miloosterman <miloosterman@protonmail.com>
2023-01-19 13:12:30 -0700

    Built board population function

--- Battleship.java
+++ Battleship.java
@@ -1,13 +1,44 @@
+import java.util.Scanner;
 
 public class Battleship{
     public static void main(String[] args){
         String[][] gameBoard = {
-            {"A0", "A1", "A2", "A3", "A4"},
-            {"B0", "B1", "B2", "B3", "B4"},
-            {"C0", "C1", "C2", "C3", "C4"},
-            {"D0", "D1", "D2", "D3", "D4"},
-            {"E0", "E1", "E2", "E3", "E4"},
+            {"█", "█", "█", "█", "█"},
+            {"█", "█", "█", "█", "█"},
+            {"█", "█", "█", "█", "█"},
+            {"█", "█", "█", "█", "█"},
+            {"█", "█", "█", "█", "█"},
         };
-        System.out.print(gameBoard[4][4]);
+        String [][] playerBoard = chooseBoardPos(gameBoard);
+        printBoard(playerBoard);
     }
+
+    private static String[][] chooseBoardPos(String[][] board){
+        int numShips = 2;
+        int shipSize = 3;
+
+        Scanner input = new Scanner(System.in);
+        for (int i=1;i<=numShips;i++){
+            System.out.println("Place ship number " + i);
+            for (int j=0;j<shipSize;j++){
+                System.out.println("Enter the row and column (ex. B2)");
+                String userPlacement = input.nextLine();
+                int xAxis = (int)userPlacement.charAt(0) - 65;
+                int yAxis = Character.getNumericValue(userPlacement.charAt(1));
+                board[xAxis][yAxis] = "C";
+            }
+        }
+        input.close();
+        return board;
+    }
+
+    private static void printBoard(String[][] board){
+        for (int i=0;i<board.length;i++){
+            for (int j=0; j<board[i].length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
