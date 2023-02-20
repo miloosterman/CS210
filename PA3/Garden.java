0b8311f2c371fbc0e738ddc0bf81f721e1d74a2e
miloosterman <miloosterman@protonmail.com>
2023-02-20 15:20:31 -0700

    Added string methods

--- PA3/Garden.java
+++ PA3/Garden.java
@@ -1,21 +1,29 @@
 public class Garden{
 
-    int rows;
-    int cols;
-    Plant[][] plants;
+    private int rows;
+    private int cols;
+    private Plant[][] plants;
 
     public Garden(int rows, int cols){
         this.rows = rows;
         this.cols = cols;
         this.plants = new Plant[rows][cols];
-    }
-
-    public void print(){
         for (int i = 0; i < plants.length; i++){
             for (int j = 0; j < plants[i].length; j++){
-                System.out.print(plants[i][j]);
+                this.plants[i][j] = new Plant();
             }
-            System.out.print("\n");
         }
+    }
+
+    public String toString(){
+        String res = "";
+        for (int i=0; i<plants.length;i++){
+            for (int j=0; j<plants[i].length;j++){
+                res += plants[i][j].printRow(j);
+            }
+            res += "\n";
+        }
+
+        return res;
     }
 }
--- PA3/Plant.java
+++ PA3/Plant.java
@@ -1,8 +1,23 @@
 //Create abstract methods in this class that will grow/harvest/etc
+//Compare type to ENUMS and use overridden pick function
 public class Plant {
-    static final int CROP_SIZE = 5;
-
+    private static final int CROP_SIZE = 5;
+    private char[][] crop = new char[CROP_SIZE][CROP_SIZE];
+    
     public Plant(){
-        
+        for (int i = 0; i < crop.length; i++){
+            for (int j = 0; j < crop[i].length; j++){
+                this.crop[i][j] = '.';
+            }
+        }
     }
+
+    public String printRow(int row){
+        String res = "";
+        for (char c : this.crop[row]){
+            res += c;
+        }
+
+        return res;
+}
 }
