6b10771f6a3a9e6da81622866770adb9ed92862d
miloosterman <miloosterman@protonmail.com>
2023-02-22 13:22:54 -0700

    Refactored Plant class to work with Garden class

--- PA3/Plant.java
+++ PA3/Plant.java
@@ -1,13 +1,14 @@
 //Create abstract methods in this class that will grow/harvest/etc
-//Compare type to ENUMS and use overridden pick function
 public class Plant {
-    private static final int CROP_SIZE = 5;
-    private char[][] crop = new char[CROP_SIZE][CROP_SIZE];
+    private final char SOIL = '.';
+    private final int CROP_SIZE = 5;
+
+    protected char[][] crop = new char[CROP_SIZE][CROP_SIZE];
     
     public Plant(){
         for (int i = 0; i < crop.length; i++){
             for (int j = 0; j < crop[i].length; j++){
-                this.crop[i][j] = '.';
+                this.crop[i][j] = SOIL;
             }
         }
     }
