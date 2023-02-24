311dcb704dd9fe26fdf380535cb26e093bd0ed6e
miloosterman <miloosterman@protonmail.com>
2023-02-24 10:47:59 -0700

    Restructured Plant object

--- PA3/Plant.java
+++ PA3/Plant.java
@@ -1,24 +1,29 @@
 //Create abstract methods in this class that will grow/harvest/etc
-public class Plant {
-    private final char SOIL = '.';
-    private final int CROP_SIZE = 5;
+public abstract class Plant {
+    protected final char SOIL = '.';
+    protected final int CROP_SIZE = 5;
 
-    protected char[][] crop = new char[CROP_SIZE][CROP_SIZE];
-    
-    public Plant(){
-        for (int i = 0; i < crop.length; i++){
-            for (int j = 0; j < crop[i].length; j++){
+    protected char[][] crop;
+
+    public Plant() {
+        this.crop = new char[CROP_SIZE][CROP_SIZE];
+        for (int i = 0; i < this.crop.length; i++) {
+            for (int j = 0; j < this.crop[i].length; j++) {
                 this.crop[i][j] = SOIL;
             }
         }
     }
-
-    public String printRow(int row){
+    /**
+     * Print the row that is passed as an argument from crop
+     * @param row
+     * @return res
+     */
+    public String printRow(int row) {
         String res = "";
-        for (char c : this.crop[row]){
+        for (char c : this.crop[row]) {
             res += c;
         }
 
         return res;
-}
+    }
 }
