d1d5cffb61d18883c8e80dff9daa1ebd04876de4
miloosterman <miloosterman@protonmail.com>
2023-02-24 11:16:08 -0700

    Created grow methods for Trees and Vegetables

--- PA3/Trees.java
+++ PA3/Trees.java
@@ -1,3 +1,17 @@
-public class Trees {
-    static final String[] TREES = {"oak", "willow", "banana", "coconut", "pine"};
+public class Trees extends Plant {
+    private char firstLetter;
+
+    public Trees(char firstLetter) {
+        this.crop[4][2] = firstLetter;
+        this.firstLetter = firstLetter;
+    }
+
+    public void grow() {
+        for (int i = 0; i < this.crop.length; i++){
+            if (this.crop[i][2] != SOIL && i-1 >= 0){
+                this.crop[i-1][2] = firstLetter;
+                break;
+            }
+        }
+    }
 }
--- PA3/Vegetables.java
+++ PA3/Vegetables.java
@@ -1,3 +1,17 @@
-public class Vegetables {
-    static final String[] VEGETABLES = {"garlic", "zucchini", "tomato", "yam", "lettuce"};
+public class Vegetables extends Plant {
+    private char firstLetter;
+
+    public Vegetables(char firstLetter){
+        this.crop[0][2] = firstLetter;
+        this.firstLetter = firstLetter;
+    }
+
+    public void grow() {
+        for (int i = CROP_SIZE-1; i < this.crop.length; i--){
+            if (this.crop[i][2] != SOIL && i+1 <= CROP_SIZE){
+                this.crop[i+1][2] = firstLetter;
+                break;
+            }
+        }
+    }
 }
